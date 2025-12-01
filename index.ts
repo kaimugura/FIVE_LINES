10fba03e9741bb495eb00b4a2f6bec13b70020db
kaimugura <hiro6garssroots@gmail.com>
2025-12-01 19:53:13 +0900

    draw()関数をリファクタリング

--- index.ts
+++ index.ts
@@ -112,15 +112,22 @@ function update() {
   }
 }
 
+
 function draw() {
+  let g = createGraphics();
+  drawMap(g);
+  drawPlayer(g);
+}
+
+function createGraphics(){
   let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
   let g = canvas.getContext("2d");
 
   g.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawMap(g);
-  drawPlayer(g);
+  return g;
 }
+
 
 function drawMap(g: CanvasRenderingContext2D){
   // Draw map
