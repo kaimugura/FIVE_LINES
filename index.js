ab6d7c3e1055773444020bf7f6f664c3ac1a7b03
kaimugura <hiro6garssroots@gmail.com>
2025-12-02 19:10:26 +0900

    Complete Chapter3

--- index.js
+++ index.js
@@ -85,44 +85,60 @@ function moveVertical(dy) {
     }
 }
 function update() {
+    handleInputs();
+    updateMap();
+}
+function handleInputs() {
     while (inputs.length > 0) {
         var current = inputs.pop();
-        if (current === Input.LEFT)
-            moveHorizontal(-1);
-        else if (current === Input.RIGHT)
-            moveHorizontal(1);
-        else if (current === Input.UP)
-            moveVertical(-1);
-        else if (current === Input.DOWN)
-            moveVertical(1);
+        handleInput(current);
     }
+}
+function handleInput(input) {
+    if (input === Input.LEFT)
+        moveHorizontal(-1);
+    else if (input === Input.RIGHT)
+        moveHorizontal(1);
+    else if (input === Input.UP)
+        moveVertical(-1);
+    else if (input === Input.DOWN)
+        moveVertical(1);
+}
+function updateMap() {
     for (var y = map.length - 1; y >= 0; y--) {
         for (var x = 0; x < map[y].length; x++) {
-            if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-                && map[y + 1][x] === Tile.AIR) {
-                map[y + 1][x] = Tile.FALLING_STONE;
-                map[y][x] = Tile.AIR;
-            }
-            else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-                && map[y + 1][x] === Tile.AIR) {
-                map[y + 1][x] = Tile.FALLING_BOX;
-                map[y][x] = Tile.AIR;
-            }
-            else if (map[y][x] === Tile.FALLING_STONE) {
-                map[y][x] = Tile.STONE;
-            }
-            else if (map[y][x] === Tile.FALLING_BOX) {
-                map[y][x] = Tile.BOX;
-            }
+            updateTile(x, y);
         }
     }
 }
+function updateTile(x, y) {
+    if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
+        && map[y + 1][x] === Tile.AIR) {
+        map[y + 1][x] = Tile.FALLING_STONE;
+        map[y][x] = Tile.AIR;
+    }
+    else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
+        && map[y + 1][x] === Tile.AIR) {
+        map[y + 1][x] = Tile.FALLING_BOX;
+        map[y][x] = Tile.AIR;
+    }
+    else if (map[y][x] === Tile.FALLING_STONE) {
+        map[y][x] = Tile.STONE;
+    }
+    else if (map[y][x] === Tile.FALLING_BOX) {
+        map[y][x] = Tile.BOX;
+    }
+}
 function draw() {
+    var g = createGraphics();
+    drawMap(g);
+    drawPlayer(g);
+}
+function createGraphics() {
     var canvas = document.getElementById("GameCanvas");
     var g = canvas.getContext("2d");
     g.clearRect(0, 0, canvas.width, canvas.height);
-    drawMap(g);
-    drawPlayer(g);
+    return g;
 }
 function drawMap(g) {
     // Draw map
